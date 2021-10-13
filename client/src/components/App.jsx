1f8579943bd12920129062c83373661dbc5c46d6
randallwstanford <randallwstanford@gmail.com>
2021-10-13 12:25:58 -0600

    Styles for app.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -4,7 +4,7 @@ import IngredientsAndCocktails from '../components/IngredientsAndCocktails.jsx';
 export default function App() {
   return (
     <div className="container">
-      <h1>What cocktails can I make with my current ingredients?</h1>
+      <div className="title">What cocktails can I make with my current ingredients?</div>
       <h2>Try: Sugar, Orange, Lime, Vodka, Gin, Pineapple Juice, Cherry, Olive</h2>
       <h2>Angostura bitters, Coca-Cola, Lemon Peel, Dry Vermouth, Ice, and more...</h2>
       <IngredientsAndCocktails />
