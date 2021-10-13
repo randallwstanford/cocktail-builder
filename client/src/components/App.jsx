dde13dc9d3b0766bbac866cea9e43010aa17b7b0
randallwstanford <randallwstanford@gmail.com>
2021-10-13 11:07:46 -0600

    Moved examples to top but under header.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Input from '../components/Input.jsx';
+import IngredientsAndCocktails from '../components/IngredientsAndCocktails.jsx';
 
 export default function App() {
   return (
