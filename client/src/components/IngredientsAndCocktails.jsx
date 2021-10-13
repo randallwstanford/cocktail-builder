0b4056534be01cb37e52eeb9e41e30488d5669c0
randallwstanford <randallwstanford@gmail.com>
2021-10-13 11:08:22 -0600

    Implements checker for non-work friendly words and functions to utils folder.

--- client/src/components/IngredientsAndCocktails.jsx
+++ client/src/components/IngredientsAndCocktails.jsx
@@ -1,13 +1,6 @@
 import React, { useState, useEffect } from 'react';
-const axios = require('axios');
-
-function shuffleArray(arr) {
-  for (let i = arr.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [arr[i], arr[j]] = [arr[j], arr[i]];
-  }
-  return arr;
-}
+import axios from 'axios';
+import { shuffleArray, checkForNaughtyWords, setMaxCocktails } from './utils.js';
 
 export default function IngredientsAndCocktails() {
   const [ingredients, setIngredients] = useState([]);
@@ -19,26 +12,25 @@ export default function IngredientsAndCocktails() {
   };
 
   var getCocktails = () => {
-    var params = ingredients[0];
-    if (ingredients.length > 0) params = ingredients.join(',');
-    axios.get('/drinks', { params: {ingredient: params}})
+    var parameters = ingredients[0];
+    if (ingredients.length > 0) parameters = ingredients.join(',');
+    axios.get('/drinks', { params: {ingredient: parameters}})
     .then(response => {
       if (typeof response.data.drinks !== 'string') {
-        console.log(response.data.drinks)
-        setCocktails(shuffleArray(response.data.drinks))
+        setCocktails(shuffleArray(checkForNaughtyWords(response.data.drinks)))
       }
     })
     .catch(err => console.log(err));
-  }
+  };
 
   const addIngredient = (event) => {
     event.preventDefault();
     var ingredient = event.target.ingredient.value;
     if (ingredient.length !== 0) {
-      setIngredients([...ingredients, ingredient.charAt(0).toUpperCase() + ingredient.slice(1)])
+      setIngredients([...ingredients, ingredient.charAt(0).toUpperCase() + ingredient.slice(1)]);
       document.getElementsByClassName("addIngredient")[0].value = '';
     }
-  }
+  };
 
   useEffect(() => {
     getCocktails();
@@ -51,12 +43,21 @@ export default function IngredientsAndCocktails() {
         <h2>Cocktails!</h2>
           <div className="allCocktails">
             {(cocktails === "None Found")
-              ? <div>No Cocktails</div>
-              : <div>
-                  {console.log(cocktails.length)}
-                  <h3>Random {cocktails.length} cocktails:</h3>
-                  {cocktails.slice(0, cocktails.length).map((cocktail) => { return ( <h3>{cocktail.strDrink}</h3>) } ) }
-                </div>}
+              ? <div>No Cocktails</div> :
+              <div>
+                <h3>Found {setMaxCocktails(cocktails.length)} cocktails:</h3>
+                <div className="test">
+                {cocktails.slice(0, setMaxCocktails(cocktails.length)).map((cocktail) => {
+                  return (
+                    <div className="renderedCocktail">
+                      <div className="cocktail">{cocktail.strDrink}</div>
+                      <img src={cocktail.strDrinkThumb} width="125px"/>
+                    </div>
+                  )
+                })}
+                </div>
+              </div>
+            }
           </div>
         </div>
       </div>
@@ -67,7 +68,7 @@ export default function IngredientsAndCocktails() {
             <button className="ingredient-button" onClick={clearIngredients}>Clear Ingredients</button>
             <button className="ingredient-button" type="submit">Add Ingredient</button>
           </div>
-          <input type="text" placeholder="add ingredients here!" className="addIngredient" name="ingredient"></input>
+          <input className="addIngredient" type="text" placeholder="add ingredients here!" name="ingredient"></input>
         </form>
         <div> {ingredients.map((ingredient) => { return (<h3 className="ingredient">{ingredient}</h3>) })} </div>
       </div>
