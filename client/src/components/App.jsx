38431dc30d11bf8fd8cd380039e12b356229160c
randallwstanford <randallwstanford@gmail.com>
2021-10-12 22:56:51 -0600

    Adds Header, Ingredient Examples, and IngredientsAndCocktails component.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Input from '../components/Input.jsx';
 
 export default function App() {
   return (
-    <div>
-      hello!
+    <div className="container">
+      <h1>What cocktails can I make with my current ingredients?</h1>
+      <h2>Try: Sugar, Orange, Lime, Vodka, Gin, Pineapple Juice, Cherry, Olive</h2>
+      <h2>Angostura bitters, Coca-Cola, Lemon Peel, Dry Vermouth, Ice, and more...</h2>
+      <IngredientsAndCocktails />
     </div>
   )
 };
