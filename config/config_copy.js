4e8945eb36268c263281c1df8676bf66b86ec0d6
randallwstanford <randallwstanford@gmail.com>
2022-02-02 16:19:00 -0700

    Refactored all.

--- config/config_copy.js
+++
@@ -1,3 +0,0 @@
-const token = 'xxxxxxx';
-
-module.exports = token;
--- server/index.js
+++ server/index.js
@@ -1,19 +1,21 @@
 const axios = require('axios');
 const path = require('path');
 const express = require('express');
+
 const app = express();
 const port = 3001;
-const key = require('../config/config');
+const key = require('../apiKey');
+
 const url = `https://www.thecocktaildb.com/api/json/v2/${key}/filter.php?i=`;
 
-app.use(express.static(path.join(__dirname, '../', 'client', 'dist')))
+app.use(express.static(path.join(__dirname, '../', 'client', 'dist')));
 
 app.get('/drinks', (req, res) => {
   axios.get(url + req.query.ingredient)
-    .then(response => res.send(response.data))
-    .catch(error => console.log(error))
+    .then((response) => res.send(response.data))
+    .catch((error) => console.log(error));
 });
 
 app.listen(port, () => {
   console.log(`listening on port: ${port}`);
-})
+});
